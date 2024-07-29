cd6bdacad099d4dd29ff8c29c2425157baa88ec6
hunwonlee <105646313+hunwonlee@users.noreply.github.com>
2024-07-29 16:37:28 +0900

    Revert "주문내역 디자인 및 Rectangle 수정"
    This reverts commit 29e24f5d45f484e81047557b567ff73efafd0f86.

--- src/components/MyOrder.jsx
+++ src/components/MyOrder.jsx
@@ -1,6 +1,7 @@
 import Header from './Header';
+import CartTotal from './CartTotal';
 import CartIcon from './icons/CartIcon';
-import MyOrderList from './MyOrderList';
+import CartDetail from './CartDetail';
 
 export default function MyOrder() {
   return (
@@ -9,10 +10,11 @@ export default function MyOrder() {
       <div className="flex flex-col min-h-screen bg-white py-12 px-20">
         <div className="flex items-center mb-8 mt-2">
           <CartIcon className="w-12 h-12 mr-3" />
-          <h2 className="text-4xl font-semibold">OO님의 주문내역</h2>
+          <h2 className="text-4xl font-semibold">OO님의 장바구니</h2>
         </div>
         <div className="w-full mx-auto flex gap-8 bg-white">
-          <MyOrderList />
+          {/* 왼쪽: 카트 정보 */}
+          <CartDetail />
         </div>
       </div>
     </div>
