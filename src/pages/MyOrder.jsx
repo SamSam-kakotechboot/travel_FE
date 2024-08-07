c38f8f07991ce37b906ef1b1b2b6ecfb97eb59d5
hunwonlee <105646313+hunwonlee@users.noreply.github.com>
2024-08-07 11:58:12 +0900

    feat : myOrder 페이지 복구 && 로그아웃 기능 구현

--- src/pages/MyOrder.jsx
+++ src/pages/MyOrder.jsx
@@ -1,19 +1,36 @@
-import Header from '../components/Header';
-import CartTotal from '../components/CartTotal';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import CartIcon from '../components/icons/CartIcon';
-import CartDetail from '../components/CartDetail';
+import MyOrderList from '../components/MyOrderList';
+import { eraseCookie } from '../utils/cookie';
+import { clearCredentials } from '../store/authSlice';
 
 export default function MyOrder() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(clearCredentials());
+    eraseCookie('token');
+    navigate('/');
+  };
+
   return (
     <div className="relative bg-white">
-      <div className="flex flex-col min-h-screen bg-white py-12 px-20">
+      <div className="flex flex-col min-h-screen bg-white p-8">
         <div className="flex items-center mb-8 mt-2">
           <CartIcon className="w-12 h-12 mr-3" />
-          <h2 className="text-4xl font-semibold">OO님의 장바구니</h2>
+          <h2 className="text-4xl font-semibold">OO님의 주문내역</h2>
+          <button
+            onClick={handleLogout}
+            className="ml-auto bg-red-500 text-white px-4 py-2 rounded"
+          >
+            Logout
+          </button>
         </div>
         <div className="w-full mx-auto flex gap-8 bg-white">
-          {/* 왼쪽: 카트 정보 */}
-          <CartDetail />
+          <MyOrderList />
         </div>
       </div>
     </div>
