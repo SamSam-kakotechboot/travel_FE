f16c7b1e2391373568a4bec4265332280ffaf428
Ji-soo708 <dlawotn3@naver.com>
2024-08-08 14:59:23 +0900

    refact: 마이페이지 디자인 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -18,10 +18,10 @@ export default function MyPage() {
 
   return (
     <div className="relative bg-white">
-      <div className="flex flex-col min-h-screen bg-white p-8">
+      <div className="flex flex-col min-h-screen bg-white py-12 px-20">
         <div className="flex items-center mb-8 mt-2">
-          <CartIcon className="w-12 h-12 mr-3" />
-          <h2 className="text-4xl font-semibold">OO님의 주문내역</h2>
+          <CartIcon className="w-8 h-8 mr-3" />
+          <h2 className="text-2xl font-semibold">OO님의 주문내역</h2>
           <button
             onClick={handleLogout}
             className="ml-auto bg-red-500 text-white px-4 py-2 rounded"
@@ -29,7 +29,7 @@ export default function MyPage() {
             Logout
           </button>
         </div>
-        <div className="w-full mx-auto flex gap-8 bg-white">
+        <div className="w-4/5 mx-2 flex gap-8 bg-white">
           <MyOrderList />
         </div>
       </div>
