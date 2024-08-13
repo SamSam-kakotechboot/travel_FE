5668e71271016542627ab1ee1f4ca8bd9d7070de
hunwonlee <105646313+hunwonlee@users.noreply.github.com>
2024-08-13 13:26:18 +0900

    api 연결 테스트

--- src/hooks/loginApi.js
+++ src/hooks/loginApi.js
@@ -1,28 +1,31 @@
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
 export const apiRequest = (isSignUpMode, formData) => {
-  return new Promise((resolve, reject) => {
-    const mockData = [
-      {
-        id: 'user1',
-        password: '1234',
-        name: '홍길동',
-        phone: '010-1234-5678',
-      },
-    ];
+  const endpoint = isSignUpMode ? '/auth/signUp' : '/auth/login';
+  const url = `${apiUrl}${endpoint}`;
 
-    const user = mockData.find(
-      user => user.id === formData.id && user.password === formData.password
-    );
-
-    if (user) {
-      resolve({
-        token:
-          'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzIiwicm9sZSI6Ik5PUk1BTCIsIm5hbWUiOiLtmY3quLjrj5kiLCJpZCI6IjMiLCJleHAiOjIzMjEzNTU4OTR9.1v3fnVPcs-xn2nKUDYrCOxcBM86OWNGO7m-tPWm_bqg',
-        role: 'NORMAL',
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('아이디 또는 비밀번호가 일치하지 않습니다.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      return {
+        token: data.token,
+        role: data.role,
         message: `${formData.id} 로그인 되었습니다.`,
-        user: user,
-      });
-    } else {
-      reject('아이디 또는 비밀번호가 일치하지 않습니다.');
-    }
-  });
+        user: data.user,
+      };
+    })
+    .catch(error => {
+      throw error;
+    });
 };
