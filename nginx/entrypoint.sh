99bce704190f1a5bfd7ad8ec4d14499ae1b8e9d7
ChoiseU <csw980928@gmail.com>
2024-08-20 14:17:32 +0900

    fix: active_color 못가져오던 상황 해결 중...

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# 기본 색상 설정 (blue)
+# Nginx에서 사용할 수 있도록 환경 변수 설정
+echo "env ACTIVE_COLOR=${ACTIVE_COLOR};" >> /etc/nginx/nginx.conf
+
+# Nginx 설정 파일에서 ${active_color}를 사용할 수 있도록 설정
 if [ -z "$ACTIVE_COLOR" ]; then
   ACTIVE_COLOR="blue"
 fi
 
-# Nginx 시작
 nginx -g "daemon off;"
