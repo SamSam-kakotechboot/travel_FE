50bac1afb919b4649e0a598649ffd4b51b8af6e6
ChoiseU <csw980928@gmail.com>
2024-08-20 14:53:09 +0900

    fix: active_color 못가져오던 상황 해결 중...

--- Dockerfile
+++ Dockerfile
@@ -27,17 +27,16 @@ COPY --from=build /app/dist /usr/share/nginx/html
 
 # Nginx 설정 파일을 복사
 COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
-RUN chmod 644 /etc/nginx/conf.d/default.conf
 
 # 엔트리포인트 스크립트 복사
 COPY ./nginx/entrypoint.sh /usr/bin/entrypoint.sh
 RUN chmod +x /usr/bin/entrypoint.sh
 
-# Nginx 포트 노출
-EXPOSE 80
-
 # 엔트리포인트 설정
 ENTRYPOINT ["/usr/bin/entrypoint.sh"]
+
+# Nginx 포트 노출
+EXPOSE 80
 
 # Nginx 실행
 CMD ["nginx", "-g", "daemon off;"]
--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# Nginx 설정 파일에서 ${ACTIVE_COLOR}를 사용할 수 있도록 설정
 if [ -z "$ACTIVE_COLOR" ]; then
   ACTIVE_COLOR="blue"
 fi
 
-# Nginx 설정 파일에 ACTIVE_COLOR 값을 직접 삽입
-sed -i "s/\$ACTIVE_COLOR/$ACTIVE_COLOR/g" /etc/nginx/conf.d/default.conf
+if [ "$ACTIVE_COLOR" = "blue" ]; then
+  sed -i 's/listen 8098;/listen 8099;/g' /etc/nginx/conf.d/default.conf
+else
+  sed -i 's/listen 8099;/listen 8098;/g' /etc/nginx/conf.d/default.conf
+fi
 
-# Nginx 실행
 nginx -g "daemon off;"
