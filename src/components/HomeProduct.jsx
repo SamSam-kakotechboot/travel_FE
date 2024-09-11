bc2b60581fc152594b55f7a352b8628631c6275b
hunwonlee <105646313+hunwonlee@users.noreply.github.com>
2024-09-11 15:17:47 +0900

    Feat : 이미지 s3에서 직접 불러오기

--- src/components/HomeProduct.jsx
+++ src/components/HomeProduct.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import StarRating from './Star';
-import useFetchImage from '../hooks/useFetchImage'; // 커스텀 훅을 import
 
 export default function HomeProduct({ ticket }) {
   const navigate = useNavigate();
-  const apiUrl = import.meta.env.VITE_API_BASE_URL;
-
-  // useFetchImage 훅을 사용하여 이미지를 비동기적으로 불러오기
-  const { imageSrc, loading, error } = useFetchImage(
-    apiUrl,
-    `/ticket/${ticket.title}.png`
-  );
 
   const handleProductClick = () => {
     navigate(`/tickets/${ticket.ticketId}`);
@@ -23,15 +15,13 @@ export default function HomeProduct({ ticket }) {
       className="flex flex-col items-center bg-background overflow-hidden w-[300px] h-auto p-4"
     >
       <div className="w-full h-[298px]">
-        {loading && <div>이미지를 불러오는 중...</div>}
-        {error && <div>{error}</div>}
-        {!loading && !error && (
-          <img
-            className="w-full h-full object-cover rounded-xl"
-            src={imageSrc}
-            alt={ticket.title}
-          />
-        )}
+        {/* 이미지 URL을 S3에서 직접 불러오고, onerror를 사용해 오류 시 기본 이미지를 표시 */}
+        <img
+          className="w-full h-full object-cover rounded-xl"
+          src={`https://ktbsamsambucket.s3.ap-northeast-2.amazonaws.com/ticket/${ticket.title}.png`}
+          alt={ticket.title}
+          onError={e => (e.target.src = '/src/assets/no_image.png')} // 이미지 로드 실패 시 기본 이미지로 대체
+        />
       </div>
       <div className="text-black text-lg font-poppins font-bold mt-4">
         {ticket.title}
